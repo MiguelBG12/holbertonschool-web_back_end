2e0ff483e0468a5e7592da32350a4eefdf599640
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-07 16:40:42 -0500

    Function named index_range that takes two integer arguments page and page_size

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Script that return a tuple with range of the list"""
+
+
+def index_range(page, page_size):
+    """Return tuple that have first_"""
+    start_index = (page - 1) * page_size
+    last_index = page * page_size
+
+    return (start_index, last_index)
