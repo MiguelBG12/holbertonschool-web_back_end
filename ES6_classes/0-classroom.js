c399d11b874dd9d4875072a9a4829092dae0cec2
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 17:11:53 -0500

    Fixing ESLint

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -4,7 +4,7 @@ export default class ClassRoom {
   // It accepts one parameter, maxStudentsSize, which represents the maximum number of students.
   constructor(maxStudentsSize) {
     // If maxStudentsSize is not a number, throw a TypeError with an error message.
-    if (typeof maxStudentsSize !== "number") throw TypeError("Length must be a number");
+    if (typeof maxStudentsSize !== 'number') throw TypeError('Length must be a number');
     // If maxStudentsSize is a valid number, assign it to the instance variable _maxStudentsSize.
     this._maxStudentsSize = maxStudentsSize;
   }
