f271e117746c2cfc9f3d405719a8137e344a7edc
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 18:58:09 -0500

    This code define a Pricing class that represents a price with an amount and currency. Constructor to initialize the 'amount' and 'currency' properties.

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,11 @@
+// Define a Pricing class that represents a price with an amount and currency.
+export default class Pricing {
+  // Constructor to initialize the 'amount' and 'currency' properties.
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  
+}
+  
