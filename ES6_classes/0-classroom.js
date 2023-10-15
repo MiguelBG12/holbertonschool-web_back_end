08d7609bf8a175727ed9d18617d5ce964f418360
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 17:08:24 -0500

    This code defines a class called ClassRoom with a constructor that accepts a maxStudentsSize parameter. It validates whether the provided value is a number and assigns it to the _maxStudentsSize instance variable.

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+// Define a class named ClassRoom.
+export default class ClassRoom {
+  // Constructor for the ClassRoom class.
+  // It accepts one parameter, maxStudentsSize, which represents the maximum number of students.
+  constructor(maxStudentsSize) {
+    // If maxStudentsSize is not a number, throw a TypeError with an error message.
+    if (typeof maxStudentsSize !== "number") throw TypeError("Length must be a number");
+    // If maxStudentsSize is a valid number, assign it to the instance variable _maxStudentsSize.
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
