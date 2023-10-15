f666b43bd2d37e25e98bd5b9bd8b4e9b56ac2907
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 18:43:05 -0500

    Added getter for name

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,16 +1,20 @@
 // Define a class named Currency.
 export default class Currency {
-    // Constructor for the Currency class.
-    // It accepts two parameters: 'code' (string) and 'name' (string).
-    constructor(code, name) {
-      // Check if 'code' is a string, and if not, throw a TypeError.
-      if (typeof code !== 'string') throw TypeError('Code must be a string');
-      // Check if 'name' is a string, and if not, throw a TypeError.
-      if (typeof name !== 'string') throw TypeError('Name must be a string');
+  // Constructor for the Currency class.
+  // It accepts two parameters: 'code' (string) and 'name' (string).
+  constructor(code, name) {
+    // Check if 'code' is a string, and if not, throw a TypeError.
+    if (typeof code !== 'string') throw TypeError('Code must be a string');
+    // Check if 'name' is a string, and if not, throw a TypeError.
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
   
-      // Assign the 'code' and 'name' values to instance variables.
-      this._code = code;
-      this._name = name;
-    }
+    // Assign the 'code' and 'name' values to instance variables.
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
 }
   
