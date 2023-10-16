7fbc638136bf1a8c30a007465f0f6a71e957ad64
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 19:59:21 -0500

    This code define a class 'Car' to represent a car with brand, motor, and color. Constructor that initializes 'brand', 'motor', and 'color' properties.

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,8 @@
+// Define a class 'Car' to represent a car with brand, motor, and color.
+export default class Car {
+  // Constructor that initializes 'brand', 'motor', and 'color' properties.
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
