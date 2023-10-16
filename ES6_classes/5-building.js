86ec22cbdc940f365094a87fbe229a5350c254ad
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 19:13:00 -0500

    This code define a Building class representing a building with square footage. Constructor that initializes the 'sqft' property. Check if the class extending Building has overridden 'evacuationWarningMessage.'

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+// Define a Building class representing a building with square footage.
+export default class Building {
+  // Constructor that initializes the 'sqft' property.
+  constructor(sqft) {
+    // Check if the class extending Building has overridden 'evacuationWarningMessage.'
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+
+}
