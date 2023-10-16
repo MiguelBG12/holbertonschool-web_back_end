f6d983b6e2cce4331b1e89bae85a99e289252a30
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 19:21:47 -0500

    This code import the 'Building' class from the specified module. Define a 'SkyHighBuilding' class that extends the 'Building' class.

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,6 @@
+// Import the 'Building' class from the specified module.
+import Building from './5-building';
+
+// Define a 'SkyHighBuilding' class that extends the 'Building' class.
+export default class SkyHighBuilding extends Building {
+}
