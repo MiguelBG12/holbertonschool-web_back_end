cb13ce86f4dbe17ba5c43299108febf4e4b0f773
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 23:04:42 -0500

    This code import the 'Car' class from the '10-car' module and define a new class 'EVCar' that extends the 'Car' class

---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,6 @@
+// Import the 'Car' class from the '10-car' module
+import Car from './10-car';
+
+// Define a new class 'EVCar' that extends the 'Car' class
+export default class EVCar extends Car {
+}
