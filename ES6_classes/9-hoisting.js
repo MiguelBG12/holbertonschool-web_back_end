3f3f50a82f204968f28d9c055a7fa397b44f3105
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 19:56:11 -0500

    Create instances of 'StudentHolberton' for different students with their respective Holberton classes.

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -44,5 +44,13 @@ export class StudentHolberton {
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
+}
+
+// Create instances of 'StudentHolberton' for different students with their respective Holberton classes.
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 
