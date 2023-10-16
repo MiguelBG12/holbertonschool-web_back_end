307845485290ce5e3a4bec7a43d4d514a403a6cc
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 19:45:32 -0500

    This code define a class 'HolbertonClass' to represent a Holberton class with a year and location. Constructor to initialize 'year' and 'location' properties.

---
+++ ES6_classes/9-hoisting.js
@@ -0,0 +1,10 @@
+// Define a class 'HolbertonClass' to represent a Holberton class with a year and location.
+export class HolbertonClass {
+  // Constructor to initialize 'year' and 'location' properties.Constructor to initialize 'year' and 'location' properties.
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+
+}
