8a9f341e92f880af1a570c81a48b2c17910c2713
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 23:06:17 -0500

    Constructor for 'EVCar' that accepts 4 parameters: brand, motor, color, and range. Call the constructor of the base class 'Car' with the first 3 parameters. Assign the value of the 'range' parameter to the '_range' property

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -3,4 +3,11 @@ import Car from './10-car';
 
 // Define a new class 'EVCar' that extends the 'Car' class
 export default class EVCar extends Car {
+  // Constructor for 'EVCar' that accepts 4 parameters: brand, motor, color, and range
+  constructor(brand, motor, color, range) {
+    // Call the constructor of the base class 'Car' with the first 3 parameters
+    super(brand, motor, color);
+    // Assign the value of the 'range' parameter to the '_range' property
+    this._range = range;
+  }
 }
