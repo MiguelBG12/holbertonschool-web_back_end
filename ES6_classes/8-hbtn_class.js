99af36c7d65064b19f00b1efb458be8e8b05126f
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-15 19:37:25 -0500

    This code define a 'HolbertonClass' class representing a class with size and location. Constructor that initializes the 'size' and 'location' properties.

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,10 @@
+// Define a 'HolbertonClass' class representing a class with size and location.
+export default class HolbertonClass {
+  // Constructor that initializes the 'size' and 'location' properties.
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  
+}
