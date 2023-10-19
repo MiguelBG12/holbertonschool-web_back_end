760f8bb58cb28a8b7c2ca197e52931d3e8dd3f4e
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-18 20:53:34 -0500

    This code defines a function using a for...of loop to append a string to each element in an array, creating a new array.

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,5 +1,14 @@
-// Define a function to append a string to each element of an array using the map method.
+// Define a function to append a string to each element of an array using the for...of loop.
 export default function appendToEachArrayValue(array, appendString) {
-  // Utilize map to transform elements by appending a specified string, creating a new array.
-  return array.map((value) => appendString + value);
+  // Create a new array to store the modified elements.
+  const newArray = [];
+
+  // Iterate over each element in the input array using for...of.
+  for (const value of array) {
+    // Append the specified string to the current element and push it to the new array.
+    newArray.push(appendString + value);
+  }
+
+  // Return the new array with the appended values.
+  return newArray;
 }
