d8c497dd98e37a382cffd9cb71a658028b7bbf71
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-23 16:15:50 -0500

    Added code files

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+// Define a function called getResponseFromAPI
+function getResponseFromAPI() {
+    // Within this function, we will create and return a Promise.
+    return new Promise((resolve, reject) => {
+      // Here, you can perform some asynchronous operation, such as making an API request.
+      // Then, if the operation is successful, call resolve with the result.
+      // If an error occurs, call reject with the error.
+    });
+} 
