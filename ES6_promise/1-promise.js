2e6d4b57bc9694ff316314a922bf61edf5dda52b
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-23 22:04:19 -0500

    Fix eslint bugs

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,21 +1,20 @@
-// Define a function called getFullResponseFromAPI
+// Define a function called getFullResponseFromAPI. The parameter is a boolean.
 function getFullResponseFromAPI(success) {
-    // Create and return a Promise within this function.
-    return new Promise((resolve, reject) => {
-      // Check if the 'success' parameter is true
-      if (success) {
-        // If 'success' is true, resolve the Promise with an object
-        resolve({
-          status: 200,
-          body: 'Success',
-        });
-      } else {
-        // If 'success' is false, reject the Promise with an error
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
-  }
-  
-  // Export the getFullResponseFromAPI function as the default export
-  export default getFullResponseFromAPI;
-  
+  // Create and return a Promise within this function.
+  return new Promise((resolve, reject) => {
+    // Check if the 'success' parameter is true
+    if (success) {
+      // If 'success' is true, resolve the Promise with an object
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      // If 'success' is false, reject the Promise with an error
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+
+// Export the getFullResponseFromAPI function as the default export
+export default getFullResponseFromAPI;
