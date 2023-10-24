d64ce8a6285f5722c5dd683512ad91cd5619acd8
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-23 22:41:19 -0500

    Update code

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,19 +1,19 @@
 // Define a function called handleResponseFromAPI
 function handleResponseFromAPI(promise) {
-    // Always return a resolved Promise with status '200' and body 'success'
-    return promise
-      .then(() => {
-        // Log a message when the promise resolves
-        console.log('Got a response from the API');
-        return { status: '200', body: 'success' };
-      })
-      .catch(() => {
-        // Log a message when the promise rejects
-        console.log('Got a response from the API');
-        return { status: '200', body: 'success' };
-      });
-  }
-  
-  // Export the handleResponseFromAPI function as the default export of the module
-  export default handleResponseFromAPI;
-  
+  // Always return a resolved Promise with status '200' and body 'success'
+  return promise
+    .then(() => {
+      // Log a message when the promise resolves
+      console.log('Got a response from the API');
+      // Return an object with status '200' and body 'success'
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      // Return an empty Error object when the promise rejects
+      console.log('Got a response from the API'); // Log the message for rejection as well
+      return new Error();
+    });
+}
+
+// Export the handleResponseFromAPI function as the default export of the module
+export default handleResponseFromAPI;
