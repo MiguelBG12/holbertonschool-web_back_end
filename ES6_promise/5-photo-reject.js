49ff669794d68c3e2896b78e513f79df93cbe964
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-26 16:05:34 -0500

    Replace with Promise.race to resolve with the first Promise that resolves or rejects, and then we handle the result in the .then and the errors in the .catch.

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 // Define and export a function named "uploadPhoto".
-export default function uploadPhoto(fileName) {
+export function uploadPhoto(fileName) {
   // Return a promise that is rejected with an error message.
   return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
