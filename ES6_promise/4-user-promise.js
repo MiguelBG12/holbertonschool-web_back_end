65712b0df99f188ade26767fa28322b8c1d319cd
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-28 14:06:01 -0500

    Added excute permissions for files

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
-// Export a default function named 'signUpUser' that takes two parameters.
-export default function signUpUser(firstName, lastName) {
+// Define a function named 'signUpUser' that takes two parameters: 'firstName' and 'lastName'.
+function signUpUser(firstName, lastName) {
   // Return a resolved promise with an object containing the provided 'firstName' and 'lastName'.
-  return Promise.resolve({
-    firstName,
-    lastName,
-  });
+  return Promise.resolve({ firstName, lastName });
 }
+
+// Export the 'signUpUser' function as the default export of this module.
+export default signUpUser;
