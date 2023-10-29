659831453385336c6595a283014d3573f8d748d6
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-29 16:54:55 -0500

    Added 'for...of' loop structure, which can be considered more readable, and added a check for startString to not be an empty string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,15 @@
-// Define a function to return a string of set values starting with a specific string (startString)
-function cleanSet(set, startString) {
-  // Use the spread operator (...) to convert the Set to an array, then filter and map the
-  // values based on the startString.
-  return [...set]
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
-}
+// Define a function named cleanSet that formats and joins values from a Set.
+export default function cleanSet(set, startString) {
+  const newString = [];
 
-export default cleanSet;
+  // Check if startString is a non-empty string.
+  if (typeof startString !== 'string' || startString.length === 0) return '';
+
+  // Iterate through the set, format values starting with startString, and add them to newString.
+  for (const value of set) {
+    if (value && value.startsWith(startString)) newString.push(value.slice(startString.length));
+  }
+
+  // Return the formatted values joined by '-'.
+  return newString.join('-');
+}
