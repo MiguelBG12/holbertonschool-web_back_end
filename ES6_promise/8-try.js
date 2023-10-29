d7b77f31d6689f2f28f24eef97565d452b314409
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-28 23:42:23 -0500

    Fix bug in code

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,7 +1,9 @@
 export default function divideFunction(numerator, denominator) {
+  return new Promise((resolve, reject) => {
     if (denominator === 0) {
-        throw new Error("Cannot divide by 0");
+      reject(new Error('cannot divide by 0'));
     } else {
-        return numerator / denominator;
+      resolve(numerator / denominator);
     }
+  });
 }
