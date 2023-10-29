ba65c54f25f0a053194363d63eee5a8924e7bad6
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-28 23:38:09 -0500

    Update code

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,11 +1,7 @@
-// Export a function named divideFunction.
 export default function divideFunction(numerator, denominator) {
-  // Check if the denominator is equal to 0.
-  if (denominator === 0) {
-    // If the denominator is 0, throw a new error with the message "Cannot divide by 0".
-    throw new Error('Cannot divide by 0');
-  } else {
-    // If the denominator is not 0, perform the division and return the result.
-    return numerator / denominator;
-  }
+    if (denominator === 0) {
+        throw new Error("Cannot divide by 0");
+    } else {
+        return numerator / denominator;
+    }
 }
