dc2e6e854c1e4e74807eabea60b4c8e6c5ba25a7
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-29 16:38:55 -0500

    The groceriesList function creates and returns a Map with name-quantity pairs of products from the specified shopping list.

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+// Define a function groceriesList that returns a Map of groceries with name-quantity pairs.
+function groceriesList() {
+  // Create and return a new Map with the specified grocery items.
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
+
+export default groceriesList;
