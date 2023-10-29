425ab83179cd4900ecfeee83aa21b25ff8f75b88
MiguelBG12 <miguel.bg12@gmail.com>
2023-10-29 16:16:47 -0500

    A function to take an array as an argument and use the Set constructor to convert it to a set, removing duplicates and returning the resulting Set.

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,7 @@
+// Define a function setFromArray that creates a Set from an array.
+function setFromArray(array) {
+  // Use the Set constructor to convert the array into a Set.
+  return new Set(array);
+}
+
+export default setFromArray;
