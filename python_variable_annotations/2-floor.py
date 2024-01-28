994ec5ce86f5584b4ef9fa0867c4a1b02e3afccf
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-27 19:54:15 -0500

    Type-annotated function takes a float as an argument, converts it to its string representation. The type annotations indicate that the parameter is a floating-point number and the result is a string.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function floor which takes a float n as argument and
+returns the floor of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    "Function that return a string representation of float"
+    return str(n)
