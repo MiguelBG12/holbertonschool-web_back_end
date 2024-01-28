f70a8624cb77fc85c627dd85076a2252d7933670
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-27 21:02:21 -0500

    Async function task_wait_n creates a list of completion times for concurrent tasks with a max delay.

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Async function task_wait_n creates a list of
+    completion times for concurrent tasks with a max delay.
+"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """returns list"""
+    tasks = []
+    result_list = []
+    for i in range(n):
+        tasks.append(task_wait_random(max_delay))
+
+    result_list = [await task for task in asyncio.as_completed(tasks)]
+    return result_list
