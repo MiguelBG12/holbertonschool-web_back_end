24221ddd8c9d444ad32f2412da730688a182b095
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-27 19:40:27 -0500

    The type annotations on the parameters and the result indicate that the parameters must be strings and that the result will also be a string.

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A type-annotated function basic annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    "Function concat string return string"
+    result = str1 + str2
+    return result
