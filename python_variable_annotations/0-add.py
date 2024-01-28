d4076204d4a9178610dbf02edff77e2d3998a0eb
MiguelBG12 <miguel.bg12@gmail.com>
2024-01-27 19:24:36 -0500

    A function that performs a simple addition of the two float parameters and then returns the result

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Function called add that takes two float arguments and returns the result 
+of the addition
+"""
+
+
+def add(a: float, b: float) -> float:
+    "Function sum two floats"
+    result = a + b
+    return result
