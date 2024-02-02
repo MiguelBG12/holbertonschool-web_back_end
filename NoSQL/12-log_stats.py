ee8dc6196e2ae1b1da5695b40c7f0c24f3a88f4a
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-02 18:43:23 -0500

    Code improvement: Connects to MongoDB, obtains statistics on Nginx logs and closes the connection. Modular structure using a main function.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
-"""Python script to provide stats about Nginx logs in MongoDB."""
-import pymongo
+""" script that provides stats about Nginx logs stored in MongoDB """
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017')
-    db = client.logs
-    col = db.nginx
+def log_dump():
+    """ Display stats about Nginx logs in MongoDB """
+    client = MongoClient('mongodb://localhost')  # connect to the local server
 
-    num_logs = col.count_documents({})
-    print(f"{num_logs} logs")
+    db = client.logs  # acces the losgs database
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    collection = db.nginx  # acces the nginx collection
 
-    print("Methods:")
-    for method in methods:
-        count = col.count_documents({"method": method})
-        print(f"\t{method}: {count}")
+    total_logs = collection.count_documents({})  # count the total logs
 
-    get_status = col.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
+    print(f"{total_logs} logs")  # print the total number of logs
 
-    print(f"GET /status: {get_status} requests")
+    http_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']  # define a list
+
+    print('Methods:')  # header
+
+    # iterar over each http method
+    for method in http_methods:
+        # Get the ocurrences for each method
+        count = collection.count_documents({'method': method})
+        print(f'\tmethod {method}: {count}')
+
+    #  Get the number of status check logs
+    status_check_count = collection.count_documents({
+        'method': 'GET', 'path': '/status'})
+
+    print(f'{status_check_count} status check')
+
+    client.close()  # close the connection
+
+
+if __name__ == '__main__':
+    log_dump()
