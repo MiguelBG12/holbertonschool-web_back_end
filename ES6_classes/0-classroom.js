184b4355cf9e2af45ab771adb08b25caf360d449
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-08 16:01:00 -0500

    The ClassRoom class accepts an attribute in its constructor and assigns it to the property. This class is then exported using the export default syntax.

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+// The ClassRoom class accepts an attribute in its constructor and assigns it to the property.
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
