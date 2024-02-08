73090f81eeb692ee0ee3c1a329cef01fdbbae8b4
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-08 16:37:07 -0500

    It imports a class from another file and provides a function that initializes and returns instances of classrooms with different student sizes.

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+// Code is reused and several instances of classrooms with different sizes are created.
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+
+  return [room1, room2, room3];
+}
