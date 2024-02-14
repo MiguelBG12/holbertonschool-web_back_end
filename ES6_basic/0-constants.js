4520fb61bbd0cb6dfae5554c715ae90f6e356939
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-14 17:50:05 -0500

    Fix bugs

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,17 +1,15 @@
-// Function to return a string indicating preference for const
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Declare a constant variable
-  return task; // Return the constant variable
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-// Function to return a string indicating something is okay
 export function getLast() {
-  return ' is okay'; // Return a string indicating something is okay
+  return ' is okay';
 }
 
-// Function to combine two strings and return the result
 export function taskNext() {
-  let combination = 'But sometimes let'; // Declare a variable with let
-  combination += getLast(); // Concatenate the result of getLast() function
-  return combination; // Return the combined string
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
