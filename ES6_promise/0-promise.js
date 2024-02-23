4c2c905804ca7efc0d95321f0f2de1ad20dc9f1d
MiguelBG12 <miguel.bg12@gmail.com>
2024-02-23 13:34:05 -0500

    Fix eslint format

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,18 +1,17 @@
 // Define a function named getResponseFromAPI
 function getResponseFromAPI() {
-    // Return a Promise object
-    return new Promise((resolve, reject) => {
-        // Condition to check if the operation is successful
-        if (true) {
-          // If successful, resolve the Promise
-          resolve();
-        } else {
-          // If unsuccessful, reject the Promise
-          reject();
-        }
-    });
-  }
-  
-  // Export the getResponseFromAPI function as the default export
-  export default getResponseFromAPI;
-  
+  // Return a Promise object
+  return new Promise((resolve, reject) => {
+    // Condition to check if the operation is successful
+    if (true) {
+      // If successful, resolve the Promise
+      resolve();
+    } else {
+      // If unsuccessful, reject the Promise
+      reject();
+    }
+  });
+}
+
+// Export the getResponseFromAPI function as the default export
+export default getResponseFromAPI;
